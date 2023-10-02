32da2d6f69715f9570420ce3bc458da606c8396d
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-02 22:48:24 +0530

    Update AUDIT TRAIL.cs

--- AUDIT TRAIL.cs
+++ AUDIT TRAIL.cs
@@ -1,4 +1,7 @@
         public void Audit_Trail(string type, string rowId="", string Col="", string oldV="", string newV="", string userID="", string tbName = "user")
         {
-
+            using ( StreamWriter w = new StreamWriter("D:/AUDIT.csv", true){})
+            {
+                w.WriteLine($"{type},{DateTime.Now},{tbName},{rowId},{Col},{oldV},{newV},{userID}");
+            }
         }
