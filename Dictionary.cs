6503b9a7bbcc2d25dfced7032eabcd3a0bd74a8b
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-04 22:50:24 +0530

    Update Dictionary.cs

--- Dictionary.cs
+++ Dictionary.cs
@@ -7,6 +7,10 @@ string n = name[1]//Retrive by key
 
 int index;
 
-
+foreach(KeyValuePair<int,string> pair in name)
+{
+    index = pair.Key;
+    n = pair.Value;
+}    
 
 string[] nemes = name.Values;
