c509502b406024d02e85d001eb42896f3101b393
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-04 22:50:19 +0530

    Update Dictionary.cs

--- Dictionary.cs
+++ Dictionary.cs
@@ -3,4 +3,8 @@ using System.Collections.Generic;
 IDictionary<int, string> name= new Dictionary<int, string>();
 name.Add(1, "sdsd");//Append item
 
+string n = name[1]//Retrive by key
+
+
+
 string[] nemes = name.Values;
