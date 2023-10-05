71916df83dd857ad1cb62f8496edaf25c25bd65e
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-05 22:51:26 +0530

    Update Filter OpenFileDialog.cs

--- Filter OpenFileDialog.cs
+++ Filter OpenFileDialog.cs
@@ -2,5 +2,10 @@ private void Add_Files_Click(object sender, RoutedEventArgs e)
   {
     OpenFileDialog openFileDialog = new OpenFileDialog();
     openFileDialog.Multiselect = true;
-
+    openFileDialog.Filter = "All Images Files (*.png;*.jpeg;*.gif;*.jpg;*.bmp;*.tiff;*.tif)|*.png;*.jpeg;*.gif;*.jpg;*.bmp;*.tiff;*.tif";
+    openFileDialog.Filter = "PNG Portable Network Graphics (*.png)|" + "*.png";
+    openFileDialog.Filter = "JPEG File Interchange Format (*.jpg *.jpeg *jfif)|" + "*.jpg;*.jpeg;*.jfif";
+    openFileDialog.Filter = "BMP Windows Bitmap (*.bmp)|" + "*.bmp";
+    openFileDialog.Filter = "TIF Tagged Imaged File Format (*.tif *.tiff)|" + "*.tif;*.tiff";
+    openFileDialog.Filter = "GIF Graphics Interchange Format (*.gif)|" + "*.gif";
   }
