fb4fb94aa20dcd73846157defd7f85215051d6ad
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-06 22:52:05 +0530

    Update Image Store in DataBase.cs

--- Image Store in DataBase.cs
+++ Image Store in DataBase.cs
@@ -1,2 +1,5 @@
 //Table which in DB should have column as img and it's datatype must is image
 
+        SqlCommand cmd = new SqlCommand($"INSERT INTO [STU] (name, dob, hometown,gender,cricket,img) VALUES('{name}','{dob}','{hometown}','{gender}','{cricket}',@pic)", conn);
+            MemoryStream ms = new MemoryStream();
+ 
