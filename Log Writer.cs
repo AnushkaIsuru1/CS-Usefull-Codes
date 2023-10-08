91a8e67668fc2da45cca458dc9fa22dec795b7f4
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-08 22:53:45 +0530

    Update Log Writer.cs

--- Log Writer.cs
+++ Log Writer.cs
@@ -3,5 +3,6 @@
         string p = "Log.txt";
         using (StreamWriter w = new StreamWriter(p, true))
         {
+            w.WriteLine($"{DateTime.Now} : {act} : [INP] {input} : [OUT] {output} : [MTHD] {methodNm} : [CLZ] Program");
         }
     }
