b5fe3ca9143814865907346733661ecd74502f3a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-08 22:53:43 +0530

    Update Log Writer.cs

--- Log Writer.cs
+++ Log Writer.cs
@@ -1,5 +1,7 @@
     public static void log(string act = "-", string input = "-", string output = "-", string methodNm = "Main()", bool isExit = false)
     {
         string p = "Log.txt";
-
+        using (StreamWriter w = new StreamWriter(p, true))
+        {
+        }
     }
