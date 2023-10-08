febefcc31fc20050fc6adc3ff0134e1b0ca4888a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-08 22:53:40 +0530

    Update Log Writer.cs

--- Log Writer.cs
+++ Log Writer.cs
@@ -1,4 +1,5 @@
     public static void log(string act = "-", string input = "-", string output = "-", string methodNm = "Main()", bool isExit = false)
     {
-  
+        string p = "Log.txt";
+
     }
