cdf21a66898dd9be03bf4bb2043e2af79683ca7c
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-09 22:55:24 +0530

    Update MYSQL Data Insert by C#.cs

--- MYSQL Data Insert by C#.cs
+++ MYSQL Data Insert by C#.cs
@@ -3,5 +3,7 @@ using MySql.Data.MySqlClient;
             string servConf = "server=localhost; uid=root; pwd='';database=test";
             MySqlConnection conn = new MySqlConnection();
             conn.ConnectionString = servConf;
+            try {conn.Open();}
+            catch(MySqlException e){showMessage(e.ToString());}
 
             conn.Close();
