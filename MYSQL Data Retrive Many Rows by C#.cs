246902578850c927b9237553c1a5e18249813ba4
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-10 22:56:12 +0530

    Update MYSQL Data Retrive Many Rows by C#.cs

--- MYSQL Data Retrive Many Rows by C#.cs
+++ MYSQL Data Retrive Many Rows by C#.cs
@@ -1,2 +1,5 @@
 using MySql.Data.MySqlClient;
 
+            string servConf = "server=localhost; uid=root; pwd='';database=test";
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = servConf;
