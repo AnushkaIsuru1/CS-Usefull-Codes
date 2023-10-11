01299a95076983dfa2636b7e0728120468d4ce46
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-11 22:57:15 +0530

    Update MYSQL Data Retrive of 1 Row by C#.cs

--- MYSQL Data Retrive of 1 Row by C#.cs
+++ MYSQL Data Retrive of 1 Row by C#.cs
@@ -1,3 +1,6 @@
 using MySql.Data.MySqlClient;
 
             string servConf = "server=localhost; uid=root; pwd='';database=test";
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = servConf;
+
