67b5c81182cee3ca5d4c91ec7c29bb99439d3894
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-12 22:58:12 +0530

    Update Read CSV File.cs

--- Read CSV File.cs
+++ Read CSV File.cs
@@ -5,5 +5,6 @@ foreach(string inp_line in File.ReadLines(path))
 {
     inp_col = inp_line.Split(',');
     if (inp_col[3] == "") { break; }
-
+    index = int.Parse(inp_col[3]);
+    Console.WriteLine(inp_col[0].ToString());
 }            
